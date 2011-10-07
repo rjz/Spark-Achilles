11bc4a53e27f6e1bb10b02c1c7c0a914fb1c8b4e
RJ Zaworski <rj@rjzaworski.com>
2011-10-06 18:06:14 -0700

    added achilles helper to autoload

--- config/autoload.php
+++ config/autoload.php
@@ -5,7 +5,13 @@
  *	@author    RJ Zaworski <rj@rjzaworski.com>
  */
 
-// Load the Achilles helper
-$autoload['libraries'] = array('achilles');
+// Load the achilles library
+$autoload[ 'libraries' ] = array( 'achilles' );
+
+// Load the achilles config file
+$autoload[ 'config' ] = array( 'achilles' );
+
+// Load the achilles helpers
+$autoload[ 'helper' ] = array( 'achilles' );
 
 ?>
