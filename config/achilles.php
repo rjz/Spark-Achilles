128eb92ac6372f4af29e974994c77c8913f512a6
RJ Zaworski <rj@rjzaworski.com>
2011-11-30 20:00:01 -0800

    documented config options

--- config/achilles.php
+++ config/achilles.php
@@ -1,17 +1,26 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access');
 /**
- *	Autoload achilles library
+ *	Set up achilles script parameters
  *
  *	@author    RJ Zaworski <rj@rjzaworski.com>
  */
 
-// Define the class for achilles elements 
+// Define the path to achilles scripts. Most installations are fine
+// to use the default scripts in the sparks directory, but you may
+// serve them from anywhere you want. 
+//
+// If you aren't using the `achilles_scripts` helper to autogenerate 
+// <script> tags, you may safely ignore this option.
+$config[ 'achilles_path' ] = '/sparks/achilles/0.1.1/js';
+ 
+// Define the class for achilles elements. This can be set to 
+// whatever you want, but be SURE to update the `className` option
+// used in your achilles.js file.
 $config[ 'achilles_class' ] = 'achilles-able';
 
-// Define the path to achilles scripts, relative to documentroot
-$config[ 'achilles_path' ] = '/z/sparks/achilles/0.0.1/js';
-
-// Define scripts to be loaded for achilles to run
+// Define scripts required for achilles to run. These are the files
+// that will be automatically included by the `achilles_scripts`
+// helper
 $config[ 'achilles_scripts' ] = array( 
 	'achilles.js', 
 	'achilles.lib.js'
